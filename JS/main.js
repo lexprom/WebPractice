5c03160097d3af35dca938bfc0dddee6a7697513
Lex Park <lexprom009@gmail.com>
2018-05-11 09:34:12 +0400

    task3 done

--- JS/main.js
+++ JS/main.js
@@ -29,28 +29,14 @@ function deleteRepeats()
 {
     var text = document.getElementById("delete").value;
 
-    //console.log(arr[0].replace(arr[0].match(arr[1])[0],""));
-
     var regex = /[^\s.?,;:!]+/gi;
     var arrMatch = text.match(regex);
     console.table(arrMatch);
 
-    /*var match = arrMatch[0].match(arrMatch[1]);
-    console.log(match);
-    alert(text.replace(new RegExp(match,"g"),""));*/
-    var indexes =[];
-    for (var i = 0 ; i < arrMatch.lenght;i++)
-    {
-        for(var j = 1; arrMatch[i].lenght;j++)
-        {
-            if(arrMatch[i] == arrMatch[j])
-            {
-                indexes.push(i);
-            }
-            else
-            {
-                 console.log("dicj");
-            }
-        }
-    }
+    var match = arrMatch[0].match(arrMatch[1]);
+
+    console.table(arrMatch[0]);
+
+
+    alert(text.replace(new RegExp(match,"g"),""));
 }
