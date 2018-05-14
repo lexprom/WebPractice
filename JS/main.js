93ac21f9a1d3f777ce7a0ef3441010a08b50e98a
Lex Park <lexprom009@gmail.com>
2018-05-14 09:41:14 +0400

    JS task 2 done

--- JS/main.js
+++ JS/main.js
@@ -27,16 +27,58 @@ function count()
 //task2
 function deleteRepeats()
 {
-    var text = document.getElementById("delete").value;
+  var text = document.getElementById("delete").value;
+  var regex = /[^\s.?,;:!]+/gi;
+  var arrMatch = text.match(regex);
+  var firstWord ="";
+  var result = text;
 
-    var regex = /[^\s.?,;:!]+/gi;
-    var arrMatch = text.match(regex);
-    console.table(arrMatch);
+  if(arrMatch == null) { alert(result); }
+  firstWord = arrMatch[0];
 
-    var match = arrMatch[0].match(arrMatch[1]);
+  for(var j=0; j<firstWord.length; j++) {
+    var searchChar = new RegExp(firstWord[j], "i");
+    var count = 0;
+    for(var i=0; i<arrMatch.length; i++) {
+      if(searchChar.test(arrMatch[i])) {
+        count++;
+      }
+    }
 
-    console.table(arrMatch[0]);
+    if(count === arrMatch.length) {
+      result = result.replace(new RegExp(firstWord[j], "gi"), "");
+    }
+  }
+  alert(result)
+}
+
+//task3
+
+function DateParse()
+{
+  var year = document.getElementById('year').value;
+  var month = document.getElementById('month').value;
+  var day = document.getElementById('day').value;
+  var hour = document.getElementById('hour').value;
+  var minute = document.getElementById('min').value;
+  var second = document.getElementById('sec').value;
+  var mask = document.getElementById('mask').value;
+  console.log(month);
+
+  var masks =
+  {
+    yy : year = year.substring(year.length/2,year.length),
+    yyyy : year,
+    M : month = month[1],
+    MM : month,
+    MMM : month = ,
+    MMMM :
+    d : day = day[1],
+    dd : day,
+    H : if()
+
+  }
+  console.log(masks.M);
 
 
-    alert(text.replace(new RegExp(match,"g"),""));
 }
