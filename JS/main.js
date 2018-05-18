da2d4f0f32b01d0c3c1bc8e197e1a299138387d3
Lex Park <lexprom009@gmail.com>
2018-05-18 06:49:04 +0400

    fixed small bugs

--- JS/main.js
+++ JS/main.js
@@ -56,12 +56,13 @@ function deleteRepeats()
 function DateParse()
 {
   var year = document.getElementById('year').value;
-  var month = document.getElementById('month').value;
-  var day = document.getElementById('day').value;
+  var month = document.getElementById('day').value;
+  var day = document.getElementById('month').value;
   var hour = document.getElementById('hour').value;
   var minute = document.getElementById('min').value;
   var second = document.getElementById('sec').value;
   var mask = document.getElementById('mask').value;
+  
 
   //var date = new Date(year,month,day,hour,minute,second);
   //console.log(month);
@@ -72,41 +73,48 @@ function DateParse()
 
   var masks =
   {
-    yy : year.substring(year.length/2,year.length),
     yyyy : year,
-    M : month[1],
+    yy : year.substring(year.length/2,year.length),
+    MMMM : monthNames[new Date(year,month,day).getMonth()] ,
+    MMM : new Date(year,month,day).toString().split(' ')[1],
     MM : month,
-    MMM : new Date(year,day,month).toString().split(' ')[1],
-    MMMM : monthNames[new Date(year,day,month).getMonth()] ,
-    d : day[1],
+    M : month[1],
     dd : day,
-    H : new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " PM").getHours()%10,
+    d : day[1],
     HH: new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " PM").getHours(),
-    h : new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " AM").getHours()%10 ,
+    H : new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " PM").getHours()%10,
     hh :new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " AM").getHours() ,
-    m : minute[1],
+    h : new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " AM").getHours()%10 ,
     mm : minute,
-    s : second[1],
-    ss : second 
+    m : minute[1],
+    ss : second,
+    s : second[1]
   }
 
   var res = mask;
+
+  if(res.indexOf("MMMM")!=-1 || res.indexOf("MMM")!=-1) 
+    {
+    res = res.replace(/MMMM/g, masks.MMMM);
+    res = res.replace(/MMM/g, masks.MMM);
+    }
+  if(res.indexOf("MM")!=-1 || res.indexOf("M")!=-1) 
+    {
+      res = res.replace(/MM/g,masks.MM);
+      res = res.replace(/M[\s\W]/g, masks.M);
+    }
+
   res = res.replace(/yyyy/g,masks.yyyy);
   res = res.replace(/yy/g,masks.yy);
-  res = res.replace(/MMMM/g,masks.MMMM);
-  res = res.replace(/MMM/g,masks.MMM);
-  res = res.replace(/MM/g,masks.MM);
-  res = res.replace(/M/g,masks.M);
   res = res.replace(/dd/g,masks.dd);
   res = res.replace(/d/g,masks.d);
   res = res.replace(/HH/g,masks.HH);
   res = res.replace(/H/g,masks.H);
   res = res.replace(/hh/g,masks.hh);
   res = res.replace(/h/g,masks.h);
-  res = res.replace(/mm/g,masks.mm);
-  res = res.replace(/m/g,masks.m);
+  res= res.replace(/mm/g,masks.mm);
+  res= res.replace(/m/g,masks.m);
   res = res.replace(/ss/g,masks.ss);
   res = res.replace(/s/g,masks.s);
-
   alert(res);
 }
