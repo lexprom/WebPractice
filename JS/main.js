48962b67d61aac9d6678ee6413bb7f3cc596e108
Lex Park <lexprom009@gmail.com>
2018-06-10 13:04:46 +0400

    fixed output for tasks,and some errors

--- JS/main.js
+++ JS/main.js
@@ -5,13 +5,21 @@ function count()
     var text = document.getElementById("countText").value;
     var regex = /\d+\.\d+|[+*/-]|\d+/gim;
 
-    if(text.match(/=/g).length > 1)
-    {
-      alert('Too much "="');
-      throw 'Too much "="';
-    }
 
-    var arrMatch = text.substring(0,text.indexOf('=')).match(regex); // array filled matches
+    if(text.match(/=/g) != null && text.match(/=/g).length == 1)
+    {
+      var arrMatch = text.substring(0,text.indexOf('=')).match(regex); // array filled matches
+    }
+    else if(text.match(/=/g) == null)
+    {
+      var arrMatch = text.match(regex);
+    }
+    else
+    {
+      document.getElementById('resCount').value = 'Too much "="';
+    }
+    
+    
     var res = +arrMatch[0];
     arrMatch.forEach((element,i,arrMatch) => {
         switch(element)
@@ -23,21 +31,35 @@ function count()
             case "=": break;
         }
     });
-    alert(res.toFixed(2));
+    document.getElementById('resCount').value = res.toFixed(2).toString();
 }
 
 //task2
 function deleteRepeats()
 {
   var text = document.getElementById("delete").value;
+  var arrMatch;
   var regex = /[^\s.?,;:!]+/gi;
-  var arrMatch = text.match(regex);
-  var firstWord ="";
+  if(document.getElementById("delete").value == '')
+  {
+    document.getElementById("delete").value="Empty area"
+  }
+  else
+  {
+  arrMatch = text.match(regex);
+  console.table(arrMatch);
+  var firstWord;
   var result = text;
+
+  for (let index = 0; index < arrMatch.length; index++) {
+    if(arrMatch[index].match('$') || arrMatch[index].match('*'))
+    {
+      arrMatch[index] = arrMatch[index].replace('$','/$')
+    }
+  }
 
   if(arrMatch == null) { alert(result); }
   firstWord = arrMatch[0];
-
   for(var j=0; j<firstWord.length; j++) {
     var searchChar = new RegExp(firstWord[j], "i");
     var count = 0;
@@ -51,15 +73,16 @@ function deleteRepeats()
       result = result.replace(new RegExp(firstWord[j], "gi"), "");
     }
   }
-  alert(result)
+  document.getElementById('delete').value = result;
+  }
 }
 
 //task3
 function DateParse()
 {
   var year = document.getElementById('year').value;
-  var month = document.getElementById('day').value;
-  var day = document.getElementById('month').value;
+  var month = document.getElementById('month').value;
+  var day = document.getElementById('day').value;
   var hour = document.getElementById('hour').value;
   var minute = document.getElementById('min').value;
   var second = document.getElementById('sec').value;
@@ -72,21 +95,29 @@ function DateParse()
   const monthNamesShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", 
   "Aug", "Sep", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+  var date = new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute);
+
+  const MonthFix = newValue => {
+    return (match, offset, string) => {
+        const isAlone = (string.slice(offset - 1, offset) !== 'M' && string.slice(offset + match.length, offset + match.length + 1) !== 'M')
+        return isAlone ? newValue : match
+    }
+  }
 
   var masks =
   {
     yyyy : year,
     yy : year.substring(year.length/2,year.length),
-    MMMM : monthNames[new Date(year,month,day).getMonth()] ,
-    MMM : monthNamesShort[new Date(year,month,day).getMonth()],
-    MM : month,
-    M : month[1],
+    MMMM : monthNames[date.getMonth()] ,
+    MMM : monthNamesShort[date.getMonth()],
+    MM : MonthFix(month),
+    M : MonthFix(month%10),
     dd : day,
     d : day[1],
-    HH: new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " PM").getHours(),
-    H : new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " PM").getHours()%10,
-    hh :new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " AM").getHours() ,
-    h : new Date(year + "-" + month + "-" + day + " " + hour + ":" + minute  + " AM").getHours()%10 ,
+    HH : date.getHours()>=12 ? hour : new Date(date + "PM").getHours(),
+    H : date.getHours()>=12 ? hour : new Date(date + "PM").getHours(),
+    hh : date.getHours()<=12 ? hour : ((date.getHours() % 12) || 12),
+    h : date.getHours()<=12 ? hour : ((date.getHours() % 12) || 12),
     mm : minute,
     m : minute[1],
     ss : second,
@@ -107,7 +138,8 @@ function DateParse()
   res = res.replace(/m/g,masks.m);
   res = res.replace(/ss/g,masks.ss);
   res = res.replace(/s/g,masks.s);
-  res = res.replace(/MMMM/g, masks.MMMM).replace(/MMM/g, masks.MMM).replace(/MM/g,masks.MM).replace(/\bM\b/g, masks.M);
+  res = res.replace(/MMMM/g, masks.MMMM).replace(/MMM/g, masks.MMM).replace(/MM/g,masks.MM).replace(/M/g, masks.M);
   
-  alert(res);
+  document.getElementById('resParse').value = '';
+  document.getElementById('resParse').value = res;
 }
